cebd30a49acf0bdee98ffdfd5a459e5c60905f7e
Kushan kumar <piyush1220066@gmail.com>
2026-02-04 10:42:56 +0530

    Refactor comments in maxProfit function
    Updated comments to clarify the logic of the maxProfit function.

--- Day 1-10/q4.c
+++ Day 1-10/q4.c
@@ -1,21 +1,16 @@
-You are given an array prices where prices[i] is the price of a given stock on the ith day.
-You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
-code:
+/*You are given an array prices where prices[i] is the price of a given stock on the ith day.
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.*/
+
 int maxProfit(int* prices, int pricesSize) {
-    // Edge case: If there are no prices or only one day, profit is 0
     if (pricesSize <= 1) {
         return 0;
     }
-
     int minPrice = prices[0];
     int maxProfit = 0;
-
     for (int i = 1; i < pricesSize; i++) {
-        // If current price is lower than our minimum, update minPrice
         if (prices[i] < minPrice) {
             minPrice = prices[i];
         } 
-        // Otherwise, calculate potential profit and update maxProfit if it's higher
         else if (prices[i] - minPrice > maxProfit) {
             maxProfit = prices[i] - minPrice;
         }
