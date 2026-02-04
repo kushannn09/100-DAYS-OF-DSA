ff12b021187fe62c68d805fb3eed49335f50b3cd
Kushan kumar <piyush1220066@gmail.com>
2026-02-04 10:44:32 +0530

    Add C program for inserting element in array

--- Day 1-10/q1.c
+++ Day 1-10/q1.c
@@ -1,5 +1,5 @@
 /*Problem: Write a C program to insert an element x at a given 1-based position pos in an array of n integers. Shift existing elements to the right to make space.*/
-  code:
+
 #include <stdio.h>
 int main() {
     int n, pos, x;
