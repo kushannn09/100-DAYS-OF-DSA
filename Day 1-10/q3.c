04f49881b96ce764fca3d60203ee382a14aacbb5
Kushan kumar <piyush1220066@gmail.com>
2026-02-04 10:41:43 +0530

    Comment out problem statement in q3.c
    Commented out the problem statement in q3.c.

--- Day 1-10/q3.c
+++ Day 1-10/q3.c
@@ -1,4 +1,4 @@
-Problem: Write a C program to delete the element at a given 1-based position pos from an array of n integers. Shift remaining elements to the left.
+/*Problem: Write a C program to delete the element at a given 1-based position pos from an array of n integers. Shift remaining elements to the left.*/
 code:
 #include <stdio.h>
 int main() {
