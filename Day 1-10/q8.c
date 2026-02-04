3cf42d4f911c387c7178bbbb2fe89e2fb4363766
Kushan kumar <piyush1220066@gmail.com>
2026-02-04 10:49:58 +0530

    Add removeElement function to filter array elements

--- Day 1-10/q8.c
+++ Day 1-10/q8.c
@@ -1,5 +1,5 @@
 /*Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements may be changed. Then return the number of elements in nums which are not equal to val.*/
-int removeElement(int* nums, int numsSize, int val) {
+ int removeElement(int* nums, int numsSize, int val) {
     int k = 0;
     for (int i = 0; i < numsSize; i++) {
         if (nums[i] != val) {
