05f14f74e4f62707ef2407ee88a10d2bd613313f
Kushan kumar <piyush1220066@gmail.com>
2026-02-04 10:48:50 +0530

    Create q6.c

--- Day 1-10/q6.c
+++ Day 1-10/q6.c
@@ -1,5 +1,5 @@
 /*You are given an array arr[] of size n - 1 that contains distinct integers in the range from 1 to n (inclusive). This array represents a permutation of the integers from 1 to n with one element missing. Your task is to identify and return the missing element.*/
-int missingNum(int *arr, int size) {
+ int missingNum(int *arr, int size) {
     long long expected = (long long)(size + 1) * (size + 2) / 2;
     long long sum = 0;  
     for(int i = 0; i < size; i++) {
