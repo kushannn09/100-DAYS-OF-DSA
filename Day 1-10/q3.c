6ab13eee38421ce06c932e0945d8762970484cfa
Kushan kumar <piyush1220066@gmail.com>
2026-02-04 10:47:25 +0530

    Add C program to delete element from array

--- Day 1-10/q3.c
+++ Day 1-10/q3.c
@@ -1,5 +1,4 @@
 /*Problem: Write a C program to delete the element at a given 1-based position pos from an array of n integers. Shift remaining elements to the left.*/
-code:
 #include <stdio.h>
 int main() {
     int n, pos;
