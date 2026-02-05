342128b57337c09045a262eaef0e6c0d52a94d64
Kushan kumar <piyush1220066@gmail.com>
2026-02-05 09:56:12 +0530

    Add log merging program in q9.c
    Implement a program to merge two sorted logs of user arrival times into a single chronological log.

---
+++ Day 1-10/q9.c
@@ -0,0 +1,26 @@
+/*Problem: A system receives two separate logs of user arrival times from two different servers. Each log is already sorted in ascending order. Your task is to create a single chronological log that preserves the correct order of arrivals.*/
+#include <stdio.h>
+int main() {
+    int p, q, i = 0, j = 0, k = 0;
+    scanf("%d", &p);
+    int a[p];
+    for (i = 0; i < p; i++) scanf("%d", &a[i]);
+    scanf("%d", &q);
+    int b[q];
+    for (i = 0; i < q; i++) scanf("%d", &b[i]);
+    int c[p + q];
+    i = 0; j = 0;
+    while (i < p && j < q) {
+        if (a[i] < b[j])
+            c[k++] = a[i++];
+        else
+            c[k++] = b[j++];
+    }
+    while (i < p)
+        c[k++] = a[i++];
+    while (j < q)
+        c[k++] = b[j++];
+    for (i = 0; i < p + q; i++)
+        printf("%d ", c[i]);
+    return 0;
+}
