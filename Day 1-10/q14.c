1758c7bf184edb72240f62d8123772cfa214bb23
Kushan kumar <piyush1220066@gmail.com>
2026-02-07 10:04:09 +0530

    Add Fibonacci function in q14.c
    Implement recursive Fibonacci function.

---
+++ Day 1-10/q14.c
@@ -0,0 +1,6 @@
+/*The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,*/
+int fib(int n) {
+    if (n <= 1)
+        return n;
+    return fib(n - 1) + fib(n - 2);
+}
