cbb3bda3217369a08c16b8c5389d3fe069544ebd
Kushan kumar <piyush1220066@gmail.com>
2026-02-09 10:08:04 +0530

    Add function to reverse a string in-place
    Implement in-place string reversal function.

---
+++ Day 1-10/q18.c
@@ -0,0 +1,13 @@
+/*Write a function that reverses a string. The input string is given as an array of characters s.
+You must do this by modifying the input array in-place with O(1) extra memory.*/
+void reverseString(char* s, int sSize) {
+    int left = 0, right = sSize - 1;
+    char temp;
+    while (left < right) {
+        temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+        left++;
+        right--;
+    }
+}
