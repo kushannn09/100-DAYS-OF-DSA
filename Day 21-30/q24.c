0d6809ce83ac6a98b70c1c892fe8dcfe644772c5
Kushan kumar <piyush1220066@gmail.com>
2026-02-24 17:04:14 +0530

    Create q24.c

---
+++ Day 21-30/q24.c
@@ -0,0 +1,44 @@
+/*Problem: Delete First Occurrence of a Key - Implement using linked list with dynamic memory allocation.*/
+#include <stdio.h>
+#include <stdlib.h>
+struct node {
+    int data;
+    struct node *next;
+};
+int main() {
+    int n, key, i;
+    struct node *head = NULL, *temp = NULL, *newnode;
+    scanf("%d", &n);
+    for(i = 0; i < n; i++) {
+        newnode = malloc(sizeof(struct node));
+        scanf("%d", &newnode->data);
+        newnode->next = NULL;
+        if(head == NULL) {
+            head = newnode;
+            temp = newnode;
+        } else {
+            temp->next = newnode;
+            temp = newnode;
+        }
+    }
+    scanf("%d", &key);
+    struct node *curr = head, *prev = NULL;
+    while(curr != NULL) {
+        if(curr->data == key) {
+            if(prev == NULL)
+                head = curr->next;
+            else
+                prev->next = curr->next;
+            free(curr);
+            break;
+        }
+        prev = curr;
+        curr = curr->next;
+    }
+    temp = head;
+    while(temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    return 0;
+}
