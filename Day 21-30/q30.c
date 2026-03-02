a79c5da705d5ae5643bc1c01186fd28fbdb9193b
Kushan kumar <piyush1220066@gmail.com>
2026-03-02 09:59:07 +0530

    Add polynomial linked list implementation in q30.c
    Implement polynomial representation using a linked list with dynamic memory allocation.

---
+++ Day 21-30/q30.c
@@ -0,0 +1,38 @@
+/*Problem: Polynomial Using Linked List - Implement using linked list with dynamic memory allocation.*/
+#include <stdio.h>
+#include <stdlib.h>
+struct Node {
+    int coeff;
+    int exp;
+    struct Node* next;
+};
+int main() {
+    int n, i;
+    scanf("%d", &n);
+    struct Node *head = NULL, *temp = NULL, *newNode;
+    for(i = 0; i < n; i++) {
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        scanf("%d %d", &newNode->coeff, &newNode->exp);
+        newNode->next = NULL;
+        if(head == NULL) {
+            head = newNode;
+            temp = head;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+    temp = head;
+    while(temp != NULL) {
+        if(temp->exp == 0)
+            printf("%d", temp->coeff);
+        else if(temp->exp == 1)
+            printf("%dx", temp->coeff);
+        else
+            printf("%dx^%d", temp->coeff, temp->exp);
+        if(temp->next != NULL)
+            printf(" + ");
+        temp = temp->next;
+    }
+    return 0;
+}
