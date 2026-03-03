c61c0fc65556b5686732a58d5f7deef0261dd951
Kushan kumar <piyush1220066@gmail.com>
2026-03-03 14:30:15 +0530

    Add stack implementation using an array
    Implemented a stack data structure with push, pop, and display operations.

--- Day 31-40/q31.c
+++ Day 31-40/q31.c
@@ -1,1 +1,30 @@
-
+/*Problem: Implement a stack data structure using an array with the following operations: push, pop, and display.*/
+#include <stdio.h>
+int main() {
+    int stack[100];
+    int top = -1;
+    int n, type, value;
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &type);
+        if(type == 1) {
+            scanf("%d", &value);
+            top++;
+            stack[top] = value;
+        }
+        else if(type == 2) {
+            if(top == -1)
+                printf("Stack Underflow\n");
+            else {
+                printf("%d\n", stack[top]);
+                top--;
+            }
+        }
+        else if(type == 3) {
+            for(int j = top; j >= 0; j--)
+                printf("%d ", stack[j]);
+            printf("\n");
+        }
+    }
+    return 0;
+}
