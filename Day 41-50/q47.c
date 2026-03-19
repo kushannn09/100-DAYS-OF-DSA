b367f30f9d23cfb7d2d4f400e79a714d281fadc9
Kushan kumar <piyush1220066@gmail.com>
2026-03-19 11:26:01 +0530

    Add binary tree height calculation in q47.c
    Implemented a binary tree height calculation function.

---
+++ Day 41-50/q47.c
@@ -0,0 +1,35 @@
+/*Problem: Height of Binary Tree Implement the solution for this problem.*/
+#include <stdio.h>
+#include <stdlib.h>
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+int height(struct Node* root) {
+    if (root == NULL)
+        return 0;
+
+    int leftHeight = height(root->left);
+    int rightHeight = height(root->right);
+
+    return 1 + (leftHeight > rightHeight ? leftHeight : rightHeight)
+      }
+int main() {
+    struct Node* root = createNode(1);
+    root->left = createNode(2);
+    root->right = createNode(3);
+    root->left->left = createNode(4);
+    root->left->right = createNode(5);
+
+    printf("Height of the tree: %d", height(root));
+    return 0;
+}
