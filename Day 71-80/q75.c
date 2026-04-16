a49f60d8da3edcee1ac62a465a284dfd24994715
Kushan kumar <piyush1220066@gmail.com>
2026-04-16 18:35:06 +0530

    Add function to find longest zero-sum subarray
    Implement a function to find the longest contiguous subarray with a sum of zero.

---
+++ Day 71-80/q75.c
@@ -0,0 +1,37 @@
+/*Problem Statement: Given an array of integers containing both positive and negative values, find the length of the longest contiguous subarray whose sum is equal to zero.*/
+#include <stdio.h>
+int maxLen(int arr[], int n)
+{
+    int sum = 0, max_len = 0;
+
+    int hash[1000];
+    for(int i = 0; i < 1000; i++)
+        hash[i] = -1;
+
+    for(int i = 0; i < n; i++)
+    {
+        sum += arr[i];
+
+        if(sum == 0)
+            max_len = i + 1;
+
+        if(hash[sum + 500] != -1)
+        {
+            int len = i - hash[sum + 500];
+            if(len > max_len)
+                max_len = len;
+        }
+        else
+        {
+            hash[sum + 500] = i;
+        }
+    }
+    return max_len;
+}
+int main()
+{
+    int arr[] = {15, -2, 2, -8, 1, 7, 10, 23};
+    int n = 8;
+    printf("%d", maxLen(arr, n));
+    return 0;
+}
