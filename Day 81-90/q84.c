c8e6a4bc0613a7293878d0b32f0a318f9cc2b2ff
Kushan kumar <piyush1220066@gmail.com>
2026-04-25 17:32:07 +0530

    Update q84.c

--- Day 81-90/q84.c
+++ Day 81-90/q84.c
@@ -1,25 +1,25 @@
 /*Problem: Implement Insertion Sort - Implement the algorithm.*/
 #include <stdio.h>
 int main() {
-    int n, i, j, key;
+    int n;
     scanf("%d", &n);
 
     int arr[n];
-
-    for(i = 0; i < n; i++) {
+    for(int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
-    for(i = 1; i < n; i++) {
-        key = arr[i];
-        j = i - 1;
-      
-        while(j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
-            j--;
+    for(int i = 0; i < n - 1; i++) {
+        int min = i;
+        for(int j = i + 1; j < n; j++) {
+            if(arr[j] < arr[min]) {
+                min = j;
+            }
         }
-        arr[j + 1] = key;
+        int temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
     }
-    for(i = 0; i < n; i++) {
+    for(int i = 0; i < n; i++) {
         printf("%d ", arr[i]);
     }
     return 0;
